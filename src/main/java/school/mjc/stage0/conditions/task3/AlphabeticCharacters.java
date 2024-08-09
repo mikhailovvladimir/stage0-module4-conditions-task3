f756bf2023c7beb3b23c9748e85cbb8c0a321228
CtaRik <1602211@mail.ru>
2024-08-09 17:02:03 +0600

    ok

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        char lowerCaseChar = Character.toLowerCase(character);
+
+        if ((character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z')) {
+
+            if (lowerCaseChar == 'a' || lowerCaseChar == 'e' || lowerCaseChar == 'i'
+                    || lowerCaseChar == 'o' || lowerCaseChar == 'u') {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
